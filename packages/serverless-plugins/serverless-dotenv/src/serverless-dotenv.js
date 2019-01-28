3882a48661a02dd5c752361c41044134e9ba25cd
Jared Lunde <jared.lunde@gmail.com>
2019-01-28 13:23:07 -0700

    adds serverless dotenv plugin

---
+++ packages/serverless-plugins/serverless-dotenv/src/serverless-dotenv.js
@@ -0,0 +1,114 @@
+// Major credit to: https://raw.githubusercontent.com/colynb/serverless-dotenv-plugin/master/index.js
+import dotenv from 'dotenv'
+import dotenvExpand from 'dotenv-expand'
+import chalk from 'chalk'
+import fs from 'fs'
+import path from 'path'
+import minimatch from 'minimatch'
+
+
+function exclude (exclusions) {
+  return !exclusions || !exclusions.length ? () => true : function (name) {
+    for (let glob of exclusions) {
+      if (glob === '*' || minimatch(name, glob)) {
+        return false
+      }
+    }
+
+    return true
+  }
+}
+
+function assignEnv (func, variables, stageVariables) {
+  const stageEnvironment = {}
+  stageVariables.forEach(variable => stageEnvironment[variable] = variables[variable])
+
+  func.environment = {
+    ...func.environment,
+    ...stageEnvironment
+  }
+
+  return func.environment
+}
+
+module.exports = class ServerlessPlugin {
+  constructor(serverless, options) {
+    this.name = 'serverless-dotenv'
+    this.serverless = serverless
+    this.serverless.service.provider.environment =
+      this.serverless.service.provider.environment || {}
+    this.config =
+      this.serverless.service.custom && this.serverless.service.custom.dotenv
+    this.loadEnv(options)
+  }
+
+  log (msg) {
+    this.serverless.cli.log(msg, this.name)
+  }
+
+  loadEnv({stage = this.serverless.service.provider.stage}) {
+    const path =
+      this?.config?.path
+        ? path.join(this.serverless.config.servicePath, this.config.path)
+        : fs.existsSync('.env.' + stage)
+          ? '.env.' + stage
+          : '.env'
+
+    this.log(chalk.bold(path))
+    let variables = dotenvExpand(dotenv.config({path})).parsed
+
+    if (variables) {
+      // Attaches all of the environment variables to Serverless's list of environment variables
+      // so you can use them in your config.
+      Object.keys(variables).forEach(
+        key => {
+          this.log('  ⇢ ' + key)
+          this.serverless.service.provider.environment[key] = variables[key]
+        }
+      )
+
+      const {functions} = this.serverless.service
+
+      if (this?.config?.exclude) {
+        Object.keys(functions).forEach(fn => {
+          assignEnv(
+            functions[fn],
+            variables,
+            Object.keys(variables).filter(
+              Array.isArray(this.config.exclude)
+                ? exclude(this.config.exclude)
+                : exclude(this.config.exclude[fn])
+            )
+          )
+        })
+        // glob === '*' || minimatch(filename, glob) === true
+      }
+      else if (this?.config?.include) {
+        Object.keys(functions).forEach(fn => {
+          assignEnv(
+            functions[fn],
+            variables,
+            Object.keys(variables).filter(
+              (...args) =>
+                Array.isArray(this.config.include)
+                  ? !exclude(this.config.include)(...args)
+                  : !exclude(this.config.include[fn])(...args)
+            )
+          )
+        })
+      }
+      else {
+        Object.keys(functions).forEach(
+          fn => assignEnv(functions[fn], variables, Object.keys(variables))
+        )
+      }
+
+      Object.keys(functions).forEach(fn => {
+        this.log(`${chalk.bold(fn)} environment`)
+        Object.keys(functions[fn].environment || {}).forEach(key => this.log('  ⇢ ' + key))
+      })
+    } else {
+      this.log('Could not find a .env file')
+    }
+  }
+}
